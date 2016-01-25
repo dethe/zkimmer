26c6a86b5504f17d8f416022a68c7438b86b3c9a
dethe <dethe@livingcode.org>
2016-01-24 23:09:21 -0800

    rotation is kind of working

--- main.lua
+++ main.lua
@@ -1,43 +1,111 @@
+vector = require "hump.vector"
+
 local ship
 local image
 local ships
+local deg, rad, sin, cos, floor = math.deg, math.rad, math.sin, math.cos, math.floor
+
+local SPRITE_WIDTH = 36
+local SPRITE_HEIGHT = 36
+
+function round(num)
+    if num<0 then
+        x=-.5
+    else
+        x=.5
+    end
+    whole, part = math.modf(num + x)
+    return whole
+end
+
+SpaceShip = {
+    x=0,
+    y=0,
+    velocity=vector(0,0),
+    rotation=0,
+    spriteIdx=10,
+    image=nil
+}
+
+function SpaceShip:new(image, startIdx)
+    o = {}
+    setmetatable(o, self)
+    self.__index = self
+    self.startIdx = startIdx - 1
+    self.sprite = {}
+    self.image = image
+    return o
+end
+
+function SpaceShip:rotate(r)
+    self.rotation = (self.rotation + r) % 360
+    print(string.format('rotation is %s degrees',  self.rotation))
+    -- 9 is how many degrees in 1/40th of a circle
+    local spriteIdx = round((self.rotation - 4.5) / 9)
+    print(string.format("sprite index: %f", spriteIdx)) -- should always be between 1 and 40
+    self.spriteIdx = math.max(spriteIdx, 1)
+end
+
+function SpaceShip:accelerate(dV)
+    local rads = rad(self.rotation)
+    local accelVector = vector(cos(rads) * dV, sin(rads) * dV)
+    self.velocity = self.velocity + accelVector
+end
+
+function SpaceShip:move()
+    self.x = self.x + self.velocity.x
+    self.y = self.y + self.velocity.y
+end
+
 
 function love.load()
     ships = {}
     image = love.graphics.newImage('ships.png')
-    local spritemap = love.graphics.newSpriteBatch(image, 8*80, "stream")
-    local quads = {}
-    for x = 0, 640 do
+    for x = 0, 39 do
         -- there are 16 types of ship, with 40 orientations each (5 rows of 8)
-        local shipIdx = math.floor(x / 40)
-        local spriteIdx = x % 40
         if x % 40 == 0 then
-            ship = {x=0, y=0, spriteIdx=0, sprite={} }
-            ships[shipIdx] = ship
+            ship = SpaceShip:new(image, x)
+            table.insert(ships, ship)
         end
-        local i,j = 1 + (x % 8), math.floor(x/8)
-        local quad = love.graphics.newQuad(i*35, j*35, 35, 35, image:getWidth(), image:getHeight())
-        ship.sprite[spriteIdx] = quad
+        local i,j = (x % 8), floor(x/8)
+        local quad = love.graphics.newQuad(i*SPRITE_WIDTH, j*SPRITE_HEIGHT, SPRITE_WIDTH, SPRITE_HEIGHT, image:getWidth(), image:getHeight())
+        table.insert(ship.sprite, quad)
     end
-    ship = ships[0]
+    ship = ships[1]
 end
 
 function love.update(dt)
+    -- handle keyboard movement
     if love.keyboard.isDown('up') then
-        ship.y = ship.y - 100 * dt
-    end
-    if love.keyboard.isDown('down') then
-        ship.y = ship.y + 100 * dt
+        ship:accelerate(-10 * dt)
     end
     if love.keyboard.isDown('right') then
-        ship.x = ship.x + 100 * dt
+        ship:rotate(90 * dt)
     end
     if love.keyboard.isDown('left') then
-        ship.x = ship.x - 100 * dt
+        ship:rotate(-90 * dt)
     end
+    -- keep on screen
+    if ship.x > love.graphics.getWidth() then
+        ship.x = ship.x - (love.graphics.getWidth() + SPRITE_WIDTH)
+    end
+    if ship.x < -SPRITE_WIDTH then
+        ship.x = ship.x + love.graphics.getWidth()
+    end
+    if ship.y > love.graphics.getHeight() then
+        ship.y = ship.y - (love.graphics.getHeight() + SPRITE_HEIGHT)
+    end
+    if ship.y < -SPRITE_HEIGHT then
+        ship.y = ship.y + love.graphics.getHeight()
+    end
+    ship:move()
 end
 
 function love.draw()
+    print('============')
+    print(ship.spriteIdx)
+    print(ship.sprite[ship.spriteIdx])
+    print('')
     love.graphics.draw(image, ship.sprite[ship.spriteIdx], ship.x, ship.y)
 end
 
